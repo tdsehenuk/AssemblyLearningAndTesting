27ccdd8fb7d755ba0b225cff6f256c9a29500e39
Thomas Sehenuk <tdsehenuk@icloud.com>
2025-06-01 23:01:09 -0400

    subtract_function

---
+++ subtract_function.asm
@@ -0,0 +1,23 @@
+global .section
+    global _start
+
+subtract: 
+    push ebp
+    mov ebp, esp
+
+    mov eax, [ebp + 8]
+    mov ebx, [ebp + 12]
+    sub eax, ebx
+
+    pop ebp
+    ret
+
+_start:
+    push 5
+    push 8
+    call subract
+    add esp, 8
+
+    mov ebx, eax
+    mov eax, 1
+    int 0x80
